c9cced166462e04bc7e64d561140aa1013395003
NGUYEN VAN TAI <45477632+nvt1802@users.noreply.github.com>
2022-03-05 01:11:35 +0700

    update code

--- index.js
+++ index.js
@@ -5,15 +5,57 @@ require("dotenv").config()
 const server = require("http").createServer(app)
 const path = require("path")
 const port = process.env.PORT || 4000
+const socketioJwt = require("socketio-jwt")
 
-require("./src/model")
+const model = require("./src/model")
 
 app.use(express.static(path.join(__dirname, "public")))
 app.set("views", path.join(__dirname, "./src/views"))
 app.use(cors())
 app.use(express.json())
 
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+})
 
-require("./src/routes")(app)
+io.use(
+  socketioJwt.authorize({
+    secret: "access-token-secret-tainv13",
+    handshake: true,
+    // auth_header_required: true,
+  })
+)
+
+io.on("connection", (socket) => {
+  console.log('runn');
+  let listUserTyping = []
+
+  socket.on("send", async (data) => {
+    await model.Message.create(data)
+    const messages = await model.Message.findAll()
+    io.sockets.emit("send", messages)
+  })
+
+  socket.on("focusin", (data) => {
+    listUserTyping.push(data)
+    io.sockets.emit("typing", listUserTyping)
+  })
+  socket.on("focusout", (data) => {
+    listUserTyping = []
+    listUserTyping = [
+      ...listUserTyping.filter((item, idx) => {
+        return item?.username !== data?.username
+      }),
+    ]
+
+    io.sockets.emit("typing", listUserTyping)
+  })
+})
+
+
+require("./src/routes")(app, io)
 
 server.listen(port, console.log(`server runing in port ${port}`))
--- src/api/message/index.js
+++ src/api/message/index.js
@@ -1,51 +1,9 @@
 const messageRouter = require("express").Router()
 const model = require("../../model")
-const socketioJwt = require("socketio-jwt")
 
 messageRouter.get("/start", async (req, res) => {
   const messages = await model.Message.findAll()
   res.json(messages)
-})
-
-const io = require("socket.io")(process.env.PORT_SOCKET, {
-  cors: {
-    origin: "http://localhost:3000",
-    // credentials: true,
-    // allowedHeaders: ["access-token"],
-  },
-})
-
-io.use(
-  socketioJwt.authorize({
-    secret: "access-token-secret-tainv13",
-    handshake: true,
-    // auth_header_required: true,
-  })
-)
-
-io.on("connection", (socket) => {
-  let listUserTyping = []
-
-  socket.on("send", async (data) => {
-    await model.Message.create(data)
-    const messages = await model.Message.findAll()
-    io.sockets.emit("send", messages)
-  })
-
-  socket.on("focusin", (data) => {
-    listUserTyping.push(data)
-    io.sockets.emit("typing", listUserTyping)
-  })
-  socket.on("focusout", (data) => {
-    listUserTyping = []
-    listUserTyping = [
-      ...listUserTyping.filter((item, idx) => {
-        return item?.username !== data?.username
-      }),
-    ]
-
-    io.sockets.emit("typing", listUserTyping)
-  })
 })
 
 module.exports = messageRouter
